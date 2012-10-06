ab1d17b02944de317361aa8751cf2b601a141b00
Paul Bailey <paul.m.bailey@gmail.com>
2012-10-05 19:09:25 -0500

    pagination

--- pizza/blog/views.py
+++ pizza/blog/views.py
@@ -1,5 +1,8 @@
 from django.template.response import TemplateResponse
 
+from pizza.pagination import pagination
+
+@pagination('posts')
 def blog_index (request, slug=None):
   return TemplateResponse(request, ['blog/index.html', 'pizza/blog/index.html'], {})
   
