fd37be89cb64de56f5fc22346262874fa4ecc2fd
Paul Bailey <paul.m.bailey@gmail.com>
2012-10-05 19:07:55 -0500

    pagination

--- pizza/blog/views.py
+++ pizza/blog/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.template.response import TemplateResponse
+
+def blog_index (request, slug=None):
+  return TemplateResponse(request, ['blog/index.html', 'pizza/blog/index.html'], {})
+  
