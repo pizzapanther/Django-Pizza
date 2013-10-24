79cbda96b35ec60fa52a56ac3fe3bd0090c8a783
Paul Bailey <paul.m.bailey@gmail.com>
2013-10-24 15:26:38 +0000

    updated ckeditor

--- pizza/kitchen_sink/static/ks/js/ckconfig.js
+++ pizza/kitchen_sink/static/ks/js/ckconfig.js
@@ -6,15 +6,15 @@ CKEDITOR.editorConfig = function( config ) {
   [
     { name: 'document', items : [ 'Source' ] },
     { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-  	{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
-  	'/',
+  	{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-', 'Scayt' ] },
+  	//'/',
   	{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
   	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
   	'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
   	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-  	{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','Iframe' ] },
+  	{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','SpecialChar','Iframe' ] },
   	'/',
-  	{ name: 'styles', items : [ 'Styles','Format','FontSize' ] },
+  	{ name: 'styles', items : [ 'Styles','Format', 'Font', 'FontSize' ] },
   	{ name: 'colors', items : [ 'TextColor','BGColor' ] },
   	{ name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
   ];
--- pizza/kitchen_sink/widgets.py
+++ pizza/kitchen_sink/widgets.py
@@ -6,20 +6,15 @@ from django.core.urlresolvers import reverse
 class RichText (forms.Textarea):
   def render (self, name, value, attrs=None):
     ret = super(RichText, self).render(name, value, attrs=attrs)
-    ret += """
-    <script type="text/javascript">
-      CKEDITOR.replace( \'id_%s\', {customConfig : \'%sks/js/ckconfig.js\'});
-    </script>
-    """ % (
-      name,
-      settings.STATIC_URL
-    )
+    ret = ret.replace('<textarea ', '<textarea class="pizza_editor" ')
     
     return mark_safe(ret)
     
   class Media:
-    #css = {'all': ('ks/redactor/redactor.css', 'ks/redactor/admin.css')}
+    css = {'all': ('ks/css/pizza_editor.css',)}
+    
     js = (
       'ks/ckeditor/ckeditor.js',
+      'ks/js/pizza_editor.js',
     )
     
