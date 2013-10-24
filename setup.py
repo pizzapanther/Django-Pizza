1cea10fdff00048f62645f38274a3f18e49cdde6
Paul Bailey <paul.m.bailey@gmail.com>
2013-10-24 18:01:19 +0000

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Django-Pizza",
-    version = '13.10.5',
+    version = '13.10.6',
     description = "Yet another Django CMS.",
     url = "https://github.com/pizzapanther/Django-Pizza",
     author = "Paul Bailey",
